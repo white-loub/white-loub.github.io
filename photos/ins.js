f47322eea5e234e32c3e775a3cf11bca6b650583
white-loub <215702308@qq.com>
2020-07-13 11:44:44 +0800

    Site updated: 2020-07-13 11:44:43

--- photos/ins.js
+++ photos/ins.js
@@ -116,8 +116,10 @@
         var data = res.list[j].arr;
         var liTmpl = "";
         for (var i = 0, len = data.link.length; i < len; i++) {
-		  var minSrc = 'https://raw.githubusercontent.com/white-loub/white-loub.github.io/master/photos/min_photos/' + data.link[i];
-          var src = 'https://raw.githubusercontent.com/white-loub/white-loub.github.io/master/photos/photos/' + data.link[i];
+		  // var minSrc = 'https://raw.githubusercontent.com/white-loub/white-loub.github.io/master/photos/min_photos/' + data.link[i];
+          // var src = 'https://raw.githubusercontent.com/white-loub/white-loub.github.io/master/photos/photos/' + data.link[i];
+		  var minSrc = 'https://white-loub.coding.net/p/blog/d/blog/git/raw/master/photos/min_photos/' + data.link[i];
+          var src = 'https://white-loub.coding.net/p/blog/d/blog/git/raw/master/photos/photos/' + data.link[i];
           var type = data.type[i];
           var target = src + (type === 'video' ? '.mp4' : '.jpg');
           src += '';
